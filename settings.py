37fb1298b91edacb8d1d20a87dc9d0571135e14d
David Haslam <dbhaslam@gmail.com>
2024-04-13 23:19:41 -0400

    modified:   settings.py

--- settings.py
+++ settings.py
@@ -85,6 +85,9 @@ DATABASES = {
     }
 }
 
+# but seems i need to use a different password for mysql
+# username = haslamdb
+# password = Mattom0!
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
