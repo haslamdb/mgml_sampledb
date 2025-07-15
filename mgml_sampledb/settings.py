50ecabd855d94d4e4ca366c78bad86e4ac9349b5
David Haslam <dbhaslam@gmail.com>
2025-07-15 06:45:35 -0400

    modified:   mgml_sampledb/settings.py 	modified:   sampletracking/__pycache__/admin.cpython-312.pyc

--- mgml_sampledb/settings.py
+++ mgml_sampledb/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mgml-sampledb.com', 'www.mgml-sample
 # Application definition
 
 INSTALLED_APPS = [
+    'sampletracking',  # Must be before admin to override templates
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'simple_history',
-    'sampletracking',
 ]
 
 MIDDLEWARE = [
